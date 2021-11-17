416052392462f4854ba3f3ac6a81c74552fde136
janisg-tan <janisg.tan@ibm.com>
2021-11-17 22:37:23 +0800

    Adjust list Feeds data

--- app/screens/feed/feed-timeline-main.tsx
+++ app/screens/feed/feed-timeline-main.tsx
@@ -33,62 +33,18 @@ import { FeedTimelineItem, FeedTimelineItems } from "./feed.type"
 import { FeedButton } from "./component/feed-button"
 
 const FeedTimelineMain: FC<StackScreenProps<NavigatorParamList, "feedTimelineMain">> = observer(
-  ({ navigation }) => {
-    // empty list state
-    // const [coachingData, setCoachingData] = useState<Array<CoachingJournalItem>>([]);
-    // const [coachingData, setCoachingData] = useState<Array<CoachingJournalItem>>([]);
-    // const [selectedActivities, setSelectedActivities] = useState<string>('');
-    // const [, forceUpdate] = useReducer(x => x + 1, 0);
-    const { feedStore } = useStores()
+  ({ navigation, route }) => {
+   const { feedStore } = useStores()
+    const { listFeeds } = route.params
 
     // const onRefresh = React.useCallback(async() => {
     //   setCoachingData([])
     //   await coachingStore.getJournal()
     // }, []);
 
-    const goBack = () => {
-      navigation.reset({
-        routes: [{ name: "homepage" }],
-      })
-    }
+    const goBack = () => navigation.goBack()
 
     const goToNewPost = () => navigation.navigate("newPost")
-
-    const feedData: FeedTimelineItem[] = [
-      {
-        id: "0",
-        imageUrl:
-          "https://www.gstatic.com/webp/gallery/4.jpg;https://www.gstatic.com/webp/gallery/4.jpg",
-        description:
-          "Today, I’m starting a new monthly task! I designed a new strategy to nudge my team members to be more active in our monthly meetings. ",
-        author: {
-          fullname: "Mrs. Geneva Herrings",
-          nickname: "Geneva",
-          title: "Head of Accounting Dept.",
-        },
-        commentCount: 4,
-        isNew: true,
-        createdAt: "2021-09-24T10:39:39.000Z",
-        updatedAt: "2021-09-24T10:39:39.000Z",
-        comments: [],
-      },
-      {
-        id: "1",
-        imageUrl: "https://www.gstatic.com/webp/gallery/4.jpg",
-        description:
-          "Tmr, I’m starting a new monthly task! I designed a new strategy to nudge my team members to be more active in our monthly meetings. ",
-        author: {
-          fullname: "Mr. Herrings",
-          nickname: "Herri",
-          title: "Manager of Accounting Dept.",
-        },
-        commentCount: 4,
-        isNew: true,
-        createdAt: "2021-09-24T10:39:39.000Z",
-        updatedAt: "2021-09-24T10:39:39.000Z",
-        comments: [],
-      },
-    ]
 
     const getListFeed = useCallback(async () => {
       await feedStore.getListFeeds()
@@ -117,7 +73,7 @@ const FeedTimelineMain: FC<StackScreenProps<NavigatorParamList, "feedTimelineMai
                 underlineWidth={Spacing[72]}
                 text="Feed."
               />
-              {feedData.map((data) => {
+              {listFeeds.map((data) => {
                 return <FeedPost data={data} key={data.id}></FeedPost>
               })}
               <Spacer height={Spacing[8]} />
