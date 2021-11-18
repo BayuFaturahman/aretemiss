ba6cd7a912ac78ce0dcc4f9d5ef120d3eac15729
janisg-tan <janisg.tan@ibm.com>
2021-11-18 21:27:59 +0800

    WIP Create new post screen

--- app/screens/feed/new-post.tsx
+++ app/screens/feed/new-post.tsx
@@ -1,14 +1,22 @@
 import React, { FC, useCallback } from "react"
-import { RefreshControl, SafeAreaView, ScrollView, View } from "react-native"
+import {
+  Platform,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
 import { Text, BackNavigation, Button, TextField } from "@components"
-import { NavigatorParamList } from "@navigators/main-navigator"
+import { NavigatorParamList } from "@navigators/feed-navigator"
 import { HStack, VStack } from "@components/view-stack"
 import Spacer from "@components/spacer"
 import { Colors, Layout, Spacing } from "@styles"
 
 import FastImage from "react-native-fast-image"
+import { launchImageLibrary, ImagePickerResponse } from "react-native-image-picker"
 import { useStores } from "../../bootstrap/context.boostrap"
 
 import insertPict from "@assets/icons/feed/insertPict.png"
@@ -19,15 +27,53 @@ const NewPost: FC<StackScreenProps<NavigatorParamList, "newPost">> = observer(({
   // empty list state
   const { feedStore } = useStores()
 
+  const qualityImage = Platform.OS === "ios" ? 0.4 : 0.5
+  const maxWidthImage = 1024
+  const maxHeightImage = 1024
+
   // const onRefresh = React.useCallback(async() => {
   //   setCoachingData([])
   // }, []);
 
-  const goBack = () => {
-    navigation.reset({
-      routes: [{ name: "homepage" }],
-    })
-  }
+  const goBack = () => navigation.goBack()
+
+  const cameraHandler = useCallback(async (response: ImagePickerResponse) => {
+    if (!response.didCancel) {
+      const formData = new FormData()
+      // formData.append('files', response.assets[0].base64 )
+      formData.append("files", {
+        ...response.assets[0],
+        // @ts-ignore
+        uri:
+          Platform.OS === "android"
+            ? response.assets[0].uri
+            : response.assets[0].uri.replace("file://", ""),
+        name: `profile-image-${
+          response.assets[0].fileName.toLowerCase().split(" ")[0]
+        }-${new Date().getTime()}.jpeg`,
+        type: response.assets[0].type ?? "image/jpeg",
+        size: response.assets[0].fileSize,
+      })
+
+
+    } else {
+      console.log("cancel")
+    }
+  }, [])
+
+  const openGallery = useCallback(() => {
+    launchImageLibrary(
+      {
+        mediaType: "photo",
+        quality: qualityImage,
+        maxWidth: maxWidthImage,
+        maxHeight: maxHeightImage,
+        includeBase64: false,
+        selectionLimit: 1,
+      },
+      cameraHandler,
+    )
+  }, [])
 
   const getListFeed = useCallback(async () => {
     await feedStore.getListFeeds()
@@ -76,28 +122,31 @@ const NewPost: FC<StackScreenProps<NavigatorParamList, "newPost">> = observer(({
             </VStack>
             <HStack>
               <HStack>
-                <View
-                  style={{
-                    backgroundColor: Colors.MAIN_BLUE,
-                    padding: Spacing[4],
-                    borderRadius: 5,
-                    width: Spacing[36],
-                    height: Spacing[36],
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <FastImage
+                <TouchableOpacity onPress={openGallery}>
+                  <View
                     style={{
                       backgroundColor: Colors.MAIN_BLUE,
-                      height: Spacing[24],
-                      width: Spacing[24],
-                      borderRadius: Spacing[8],
+                      padding: Spacing[4],
+                      borderRadius: 5,
+                      width: Spacing[36],
+                      height: Spacing[36],
+                      justifyContent: "center",
+                      alignItems: "center",
+                      marginRight: Spacing[14],
                     }}
-                    source={insertPict}
-                    resizeMode={"contain"}
-                  />
-                </View>
+                  >
+                    <FastImage
+                      style={{
+                        backgroundColor: Colors.MAIN_BLUE,
+                        height: Spacing[24],
+                        width: Spacing[24],
+                        borderRadius: Spacing[8],
+                      }}
+                      source={insertPict}
+                      resizeMode={"contain"}
+                    />
+                  </View>
+                </TouchableOpacity>
 
                 <ScrollView
                   style={{ backgroundColor: Colors.BLACK, maxWidth: "50%" }}
@@ -129,7 +178,7 @@ const NewPost: FC<StackScreenProps<NavigatorParamList, "newPost">> = observer(({
                 text={"Update"}
                 style={{
                   height: Spacing[32],
-                  paddingHorizontal: Spacing[8],
+                  paddingHorizontal: Spacing[16],
                   right: 0,
                   position: "absolute",
                 }}
