68c259b4f88dcb3f53d376a4e019358752c57896
Arda Satata <ardasatata@gmail.com>
2021-11-23 22:01:55 +0800

    wip notification API

---
+++ app/services/api/notification/notification-api.types.ts
@@ -0,0 +1,29 @@
+import { GeneralApiProblem } from "../api-problem"
+
+export type NotificationType = "request_feedback" | "submitted_feedback" | "submitted_comment"
+
+export type NotificationItemModel = {
+  "notification_id": string;
+  "notification_content": string;
+  "notification_type": NotificationType
+  "notification_data": {
+    "journal_id": string;
+  },
+  "notification_created_at": string;
+  "notification_updated_at": string;
+  "notification_author_id": string;
+  "notification_author_photo": string;
+}
+
+export interface ErrorFormResponse {
+  errorCode: number
+  message: string
+}
+
+export interface GetListNotificationsResponse {
+  message: string
+  token: string
+  data: NotificationItemModel[]
+}
+
+export type GetNotificationsListResult = { kind: "form-error"; response: ErrorFormResponse } | { kind: "ok"; response: GetListNotificationsResponse } | GeneralApiProblem
---
+++ app/store/store.notification.ts
@@ -0,0 +1,122 @@
+/* eslint-disable max-lines */
+// #region IMPORTS
+// PACKAGES
+import { makeAutoObservable } from "mobx"
+import ServiceStore from "./store.service"
+import { Api } from "@services/api"
+import { FeedApi } from "@services/api/feed/feed-api"
+import { ErrorFormResponse, FeedApiModel } from "@services/api/feed/feed-api.types"
+import { FeedItemType } from "@screens/feed/feed.type"
+
+
+export default class NotificationStore {
+  // #region PROPERTIES
+
+  api: Api
+  isLoading: boolean
+
+  refreshData: boolean
+
+  errorCode: number
+  errorMessage: string
+
+  feedApi: FeedApi
+  listFeeds: FeedItemType[]
+
+  constructor(api: Api) {
+    this.api = api
+    this.isLoading = false
+
+    this.errorCode = null
+    this.errorMessage = null
+
+    this.listFeeds = []
+    this.feedApi = new FeedApi(this.api)
+  }
+
+  async getListFeeds() {
+    this.isLoading = true
+    try {
+      const response = await this.feedApi.getListFeeds()
+
+      if (response.kind === "form-error") {
+        this.formError(response.response)
+      }
+
+      if (response.kind === "ok") {
+        this.getListFeedsSuccess(response.response.data)
+      }
+    } catch (e) {
+      console.log(e)
+      this.setErrorMessage(e)
+    } finally {
+      console.log("getListFeeds done")
+      this.isLoading = false
+    }
+  }
+
+  getListFeedsSuccess(data: FeedApiModel[]) {
+    console.log("getListFeedsSuccess data", data)
+    this.listFeeds = []
+    data.forEach(post => {
+      this.listFeeds.push({
+        id: post.feed_id,
+        description: post.feed_description,
+        imageUrl: post.feed_images_url,
+        author: {
+          id: post.feed_author_id,
+          nickname: post.feed_author_nickname,
+          title: 'Head of something',
+          photo: post.feed_author_photo,
+        },
+        commentCount: post.feed_comment_count,
+        isNew: true,
+        createdAt: post.feed_created_at,
+        updatedAt: post.feed_updated_at,
+        isDeleted: (post.feed_is_deleted === 1),
+        deletedAt: post.feed_deleted_at
+      })
+    })
+
+    console.log("list feed: ", this.listFeeds)
+  }
+
+  clearListFeed() {
+    this.listFeeds = []
+    console.log("list feed: ", this.listFeeds)
+  }
+
+  async uploadImage(formData: FormData) {
+    console.log("Upload Photo")
+    this.isLoading = true
+    try {
+      const response = await this.feedApi.PostUploadFeedImages(formData)
+
+      console.log(response)
+
+      if (response.kind === "form-error") {
+        this.formError(response.response)
+      }
+
+      if (response.kind === "ok") {
+        return response.response;
+      }
+    } catch (e) {
+      console.log("Upload Feed Image error")
+      console.log(e)
+      this.setErrorMessage(e)
+    } finally {
+      console.log("Upload Feed Image done")
+      this.isLoading = false
+    }
+  }
+
+  formError(data: ErrorFormResponse) {
+    this.errorCode = data.errorCode
+    this.errorMessage = data.message
+  }
+
+  setErrorMessage(e: any) {
+    this.errorMessage = e
+  }
+}
