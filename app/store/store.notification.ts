4e858ce6c19e947d3227b1ead48a621d2bd499e2
Arda Satata <ardasatata@gmail.com>
2021-11-23 22:17:25 +0800

    wip notificatoin store

--- app/store/store.notification.ts
+++ app/store/store.notification.ts
@@ -2,12 +2,23 @@
 // #region IMPORTS
 // PACKAGES
 import { makeAutoObservable } from "mobx"
-import ServiceStore from "./store.service"
 import { Api } from "@services/api"
-import { FeedApi } from "@services/api/feed/feed-api"
-import { ErrorFormResponse, FeedApiModel } from "@services/api/feed/feed-api.types"
-import { FeedItemType } from "@screens/feed/feed.type"
+import { NotificationApi } from "@services/api/notification/notification-api";
+import { ErrorFormResponse } from "@services/api/feed/feed-api.types"
+import {NotificationItemModel, NotificationType} from "@services/api/notification/notification-api.types";
 
+export type NotificationItem = {
+  "id": string;
+  "content": string;
+  "type": NotificationType
+  "data": {
+    "journalId": string;
+  },
+  "createdAt": string;
+  "updatedAt": string;
+  "authorId": string;
+  "authorPhoto": string;
+}
 
 export default class NotificationStore {
   // #region PROPERTIES
@@ -20,8 +31,8 @@ export default class NotificationStore {
   errorCode: number
   errorMessage: string
 
-  feedApi: FeedApi
-  listFeeds: FeedItemType[]
+  notificationApi: NotificationApi
+  notificationsList: NotificationItem[]
 
   constructor(api: Api) {
     this.api = api
@@ -30,85 +41,57 @@ export default class NotificationStore {
     this.errorCode = null
     this.errorMessage = null
 
-    this.listFeeds = []
-    this.feedApi = new FeedApi(this.api)
+    this.notificationsList = []
+    this.notificationApi = new NotificationApi(this.api)
+
+    makeAutoObservable(this);
   }
 
-  async getListFeeds() {
+  async getListNotifications() {
     this.isLoading = true
     try {
-      const response = await this.feedApi.getListFeeds()
+      const response = await this.notificationApi.getListNotifications()
 
       if (response.kind === "form-error") {
         this.formError(response.response)
       }
 
       if (response.kind === "ok") {
-        this.getListFeedsSuccess(response.response.data)
+        this.getNotificationsSuccess(response.response.data)
       }
     } catch (e) {
       console.log(e)
       this.setErrorMessage(e)
     } finally {
-      console.log("getListFeeds done")
+      console.log("getListNotifications done")
       this.isLoading = false
     }
   }
 
-  getListFeedsSuccess(data: FeedApiModel[]) {
-    console.log("getListFeedsSuccess data", data)
-    this.listFeeds = []
-    data.forEach(post => {
-      this.listFeeds.push({
-        id: post.feed_id,
-        description: post.feed_description,
-        imageUrl: post.feed_images_url,
-        author: {
-          id: post.feed_author_id,
-          nickname: post.feed_author_nickname,
-          title: 'Head of something',
-          photo: post.feed_author_photo,
+  getNotificationsSuccess(data: NotificationItemModel[]) {
+    console.log("getNotificationsSuccess data", data)
+    this.notificationsList = []
+    data.forEach(item => {
+      this.notificationsList.push({
+        id: item.notification_id,
+        content: item.notification_content,
+        type: item.notification_type,
+        data: {
+          journalId: item.notification_data.journal_id
         },
-        commentCount: post.feed_comment_count,
-        isNew: true,
-        createdAt: post.feed_created_at,
-        updatedAt: post.feed_updated_at,
-        isDeleted: (post.feed_is_deleted === 1),
-        deletedAt: post.feed_deleted_at
+        createdAt: item.notification_created_at,
+        updatedAt: item.notification_updated_at,
+        authorId: item.notification_author_id,
+        authorPhoto: item.notification_author_photo
       })
     })
 
-    console.log("list feed: ", this.listFeeds)
+    console.log("list notifications data: ", this.notificationsList)
   }
 
-  clearListFeed() {
-    this.listFeeds = []
-    console.log("list feed: ", this.listFeeds)
-  }
-
-  async uploadImage(formData: FormData) {
-    console.log("Upload Photo")
-    this.isLoading = true
-    try {
-      const response = await this.feedApi.PostUploadFeedImages(formData)
-
-      console.log(response)
-
-      if (response.kind === "form-error") {
-        this.formError(response.response)
-      }
-
-      if (response.kind === "ok") {
-        return response.response;
-      }
-    } catch (e) {
-      console.log("Upload Feed Image error")
-      console.log(e)
-      this.setErrorMessage(e)
-    } finally {
-      console.log("Upload Feed Image done")
-      this.isLoading = false
-    }
+  clearListNotifications() {
+    this.notificationsList = []
+    console.log("list feed: ", this.notificationsList)
   }
 
   formError(data: ErrorFormResponse) {
