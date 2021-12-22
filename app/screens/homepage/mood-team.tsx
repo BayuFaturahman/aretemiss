fddcab707eb2fe28c0d12fe3eeb480a33e95c5f0
janisg-tan <janisg.tan@ibm.com>
2021-12-22 22:08:37 +0800

    Add copywriting if mood team is empty

--- app/screens/homepage/mood-team.tsx
+++ app/screens/homepage/mood-team.tsx
@@ -18,6 +18,7 @@ import sedih from "@assets/icons/mood/sedih.png"
 import sakit from "@assets/icons/mood/sakit.png"
 import kaget from "@assets/icons/mood/kaget.png"
 import { MOOD_TYPE } from "./components/mood-component"
+import { EmptyList } from "./components/empty-list"
 
 export type TeamMoodItemType = {
   userId: string
@@ -147,9 +148,9 @@ const MoodTeam: FC<StackScreenProps<NavigatorParamList, "moodTeam">> = observer(
                 showsVerticalScrollIndicator={false}
                 ItemSeparatorComponent={() => <Spacer height={Spacing[8]} />}
                 data={listTeamMood}
-                // ListEmptyComponent={()=>
-                // <EmptyList navigateTo={goBack} />
-                // }
+                ListEmptyComponent={()=>
+                <EmptyList />
+                }
                 renderItem={({ item, index }) => {
                   return (
                     <HStack style={{ justifyContent: "space-around", width: "100%"}}>
