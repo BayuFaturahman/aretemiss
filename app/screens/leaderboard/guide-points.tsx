41771d99fd20d4a2c5216b3c1f68f8087f96fe6c
janisgtan <janisg.tan@ibm.com>
2022-02-23 21:19:06 +0800

    Add guidepoint data and fix new post button

--- app/screens/leaderboard/guide-points.tsx
+++ app/screens/leaderboard/guide-points.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react"
-import { SafeAreaView, ScrollView } from "react-native"
+import React, { FC, useState } from "react"
+import { FlatList, RefreshControl, SafeAreaView, ScrollView } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
 import { NavigatorParamList } from "@navigators/main-navigator"
@@ -10,112 +10,126 @@ import { Collapse, CollapseHeader, CollapseBody } from "accordion-collapse-react
 import { Button, Text } from "@components"
 import Spacer from "@components/spacer"
 
+export type guidePointDataType = {
+  desc: string
+  score: string
+}
+
+export type guidePointType = {
+  title: string
+  data: guidePointDataType[]
+}
+
+const guidePointsData: guidePointType[] = [
+  {
+    title: "Coaching activities.",
+    data: [
+      {
+        desc: "Tambah coaching journal",
+        score: "+2",
+      },
+      { desc: "Penilaian feedback dari coachee", score: "(average score)" },
+      {
+        desc: "Rata-rata dari self-reflection feedback dan feedback dari coachee",
+        score: "(average score)",
+      },
+    ],
+  },
+  {
+    title: "Project or improvement initiatives",
+    data: [
+      { desc: "Membuat post di Feed", score: "+2" },
+      { desc: "Jumlah komen dari rekan kerja lain di post Feed", score: "(+1 /komen)" },
+      {
+        desc: "Jumlah komen yang dikirimkan di post Feed yang dibuat oleh rekan kerja lain",
+        score: "(+1 /komen)",
+      },
+    ],
+  },
+]
+
 const GuidePoints: FC<StackScreenProps<NavigatorParamList, "guidePoints">> = observer(
   ({ navigation }) => {
     // const [point, setPoint] = useState<number>(65)
+    const [guidePointData, setGuidePointData] = useState<guidePointType[]>(guidePointsData)
 
     const goBack = () => navigation.goBack()
 
+    const renderGuidePointData = (data: guidePointDataType) => {
+      return (
+        <HStack horizontal={Spacing[12]} vertical={Spacing[4]}>
+          <Text style={{width: '60%'}}>{data.desc}</Text>
+          <Spacer />
+          <VStack
+            style={{ backgroundColor: Colors.BRIGHT_BLUE, borderRadius: Spacing[24] }}
+            horizontal={Spacing[12]}
+            vertical={Spacing[4]}
+          >
+            <Text style={{ color: Colors.WHITE }}>{data.score}</Text>
+          </VStack>
+        </HStack>
+      )
+    }
+
     return (
       <VStack
-        testID="CoachingJournalMain"
+        testID="GuidePoint"
         style={{ backgroundColor: Colors.WHITE, flex: 1, justifyContent: "center" }}
       >
         <SafeAreaView style={Layout.flex}>
-          <HStack horizontal={Spacing[24]} style={{ justifyContent: "space-between" }}>
+          <HStack top={Spacing[52]} horizontal={Spacing[24]} style={{ justifyContent: "space-between" }}>
             <Text type={"left-header"}>Cara mendapatkan poin.</Text>
             {/* <Text type={"left-header"}>Cara mendapatkan poin.</Text> */}
             <Button type={"negative"} text={"Back"} onPress={goBack}></Button>
           </HStack>
           <ScrollView>
-            <Spacer height={Spacing[24]} />
-            <VStack horizontal={Spacing[24]}>
-              <Collapse>
-                <CollapseHeader>
-                  <HStack
-                    horizontal={Spacing[12]}
-                    vertical={Spacing[4]}
-                    style={{ backgroundColor: Colors.MAIN_BLUE, borderRadius: Spacing[20] }}
-                  >
-                    <Text type={"body"} style={{ color: Colors.WHITE }}>
-                      Coaching activities.
-                    </Text>
-                    <Spacer />
-                    <Text type={"body"} style={{ color: Colors.WHITE }}>
-                      ▼
-                    </Text>
-                  </HStack>
-                </CollapseHeader>
-                <CollapseBody>
-                  <HStack horizontal={Spacing[12]} vertical={Spacing[4]}>
-                    <Text>Tambah coaching journal.</Text>
-                    <Spacer />
-                    <VStack
-                      style={{ backgroundColor: Colors.BRIGHT_BLUE, borderRadius: Spacing[24] }}
-                      horizontal={Spacing[12]}
-                      vertical={Spacing[4]}
-                    >
-                      <Text style={{ color: Colors.WHITE }}>+ 2</Text>
-                    </VStack>
-                  </HStack>
-                </CollapseBody>
-              </Collapse>
-              <Spacer height={Spacing[16]} />
+            <Spacer height={Spacing[32]} />
+            {/* <VStack horizontal={Spacing[24]}> */}
+            <FlatList
+              refreshControl={
+                <RefreshControl
+                  refreshing={false}
+                  tintColor={Colors.MAIN_RED}
+                />
+              }
+              ListFooterComponent={<Spacer height={Spacing[72]} />}
+              showsVerticalScrollIndicator={false}
+              ItemSeparatorComponent={() => <Spacer height={Spacing[8]} />}
+              data={guidePointData}
+              // ListEmptyComponent={()=>
+              // <EmptyList />
+              // }
+              renderItem={({ item, index }) => {
+                return (
+                  <Collapse>
+                    <CollapseHeader>
+                      <HStack
+                        horizontal={Spacing[12]}
+                        vertical={Spacing[4]}
+                        style={{ backgroundColor: Colors.MAIN_BLUE, borderRadius: Spacing[20] }}
+                      >
+                        <Text type={"body"} style={{ color: Colors.WHITE }}>
+                          {item.title}
+                        </Text>
+                        <Spacer />
+                        <Text type={"body"} style={{ color: Colors.WHITE }}>
+                          ▼
+                        </Text>
+                      </HStack>
+                    </CollapseHeader>
+                    <CollapseBody>
+                      {item.data.map((data) => {
+                        return(renderGuidePointData(data))
+                      })}
+                    </CollapseBody>
+                  </Collapse>
+                )
+              }}
+              style={{ paddingHorizontal: Spacing[24] }}
+              keyExtractor={(item) => item.title}
+            />
+            <Spacer height={Spacing[16]} />
 
-              {/* #2 */}
-              <Collapse>
-                <CollapseHeader>
-                  <HStack
-                    horizontal={Spacing[12]}
-                    vertical={Spacing[4]}
-                    style={{ backgroundColor: Colors.MAIN_BLUE, borderRadius: Spacing[20] }}
-                  >
-                    <Text type={"body"} style={{ color: Colors.WHITE }}>
-                      Coaching activities.
-                    </Text>
-                    <Spacer />
-                    <Text type={"body"} style={{ color: Colors.WHITE }}>
-                      ▼
-                    </Text>
-                  </HStack>
-                </CollapseHeader>
-                <CollapseBody>
-                  <HStack horizontal={Spacing[12]} vertical={Spacing[4]}>
-                    <Text>Tambah coaching journal.</Text>
-                    <Spacer />
-                    <VStack
-                      style={{ backgroundColor: Colors.BRIGHT_BLUE, borderRadius: Spacing[24] }}
-                      horizontal={Spacing[12]}
-                      vertical={Spacing[4]}
-                    >
-                      <Text style={{ color: Colors.WHITE }}>+ 2</Text>
-                    </VStack>
-                  </HStack>
-                  <HStack horizontal={Spacing[12]} vertical={Spacing[4]}>
-                    <Text>Tambah coaching journal.</Text>
-                    <Spacer />
-                    <VStack
-                      style={{ backgroundColor: Colors.BRIGHT_BLUE, borderRadius: Spacing[24] }}
-                      horizontal={Spacing[12]}
-                      vertical={Spacing[4]}
-                    >
-                      <Text style={{ color: Colors.WHITE }}>+ 2</Text>
-                    </VStack>
-                  </HStack>
-                  <HStack horizontal={Spacing[12]} vertical={Spacing[4]}>
-                    <Text>Tambah coaching journal.</Text>
-                    <Spacer />
-                    <VStack
-                      style={{ backgroundColor: Colors.BRIGHT_BLUE, borderRadius: Spacing[24] }}
-                      horizontal={Spacing[12]}
-                      vertical={Spacing[4]}
-                    >
-                      <Text style={{ color: Colors.WHITE }}>+ 2</Text>
-                    </VStack>
-                  </HStack>
-                </CollapseBody>
-              </Collapse>
-            </VStack>
           </ScrollView>
         </SafeAreaView>
       </VStack>
