5453b6ab15b371054d87cb3d5cebd30028987988
Arda Satata <ardasatata@gmail.com>
2022-03-08 03:10:51 +0800

    added form page

--- app/screens/brainstorm/add-idea.tsx
+++ app/screens/brainstorm/add-idea.tsx
@@ -1,15 +1,28 @@
 import React, { FC } from "react"
-import { SafeAreaView, ScrollView } from "react-native"
+import { SafeAreaView, ScrollView, TouchableOpacity, View } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
 import { NavigatorParamList } from "@navigators/main-navigator"
-import { VStack } from "@components/view-stack"
+import { HStack, VStack } from "@components/view-stack"
 import { Colors, Layout, Spacing } from "@styles"
 
-import { BackNavigation, Button, DropDownPicker, K_OPTIONS, Text } from "@components"
+import {
+  BackNavigation,
+  Button,
+  DropDownItem,
+  DropDownPicker,
+  K_OPTIONS,
+  Text,
+  TextField,
+} from "@components"
 import Spacer from "@components/spacer"
 
 import { IOption } from "react-native-modal-selector"
+import { dimensions } from "@config/platform.config"
+import moment from "moment"
+import { typography } from "@theme"
+import { Formik } from "formik"
+import { SOFT_PURPLE } from "@styles/Color"
 
 export type AssessmentQuizItem = {
   id: string
@@ -58,16 +71,127 @@ const AddIdea: FC<StackScreenProps<NavigatorParamList, "guidePoints">> = observe
         testID="Assesment"
         style={{ backgroundColor: Colors.WHITE, flex: 1, justifyContent: "center" }}
       >
-        <SafeAreaView style={Layout.flex}>
-          <ScrollView>
-            <BackNavigation color={Colors.UNDERTONE_BLUE} goBack={goBack} />
-            <VStack horizontal={Spacing[48]} style={{ alignItems: "center" }}>
-              <Text type={"left-header"}>Add Idea</Text>
-              <Spacer height={Spacing[24]} />
-              <Spacer height={Spacing[24]} />
-            </VStack>
-          </ScrollView>
-        </SafeAreaView>
+        <Formik
+        // initialValues={journalEntryForm}
+        // validationSchema={JournalEntryTypeSchema}
+        // onSubmit={onSubmit}
+        // validate={validate}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, setFieldValue }) => (
+            <>
+              <SafeAreaView style={Layout.flex}>
+                <ScrollView>
+                  <BackNavigation color={Colors.UNDERTONE_BLUE} goBack={goBack} />
+                  <VStack horizontal={Spacing[24]}>
+                    <HStack>
+                      <Text type={"left-header"}>Tentang ide projek!</Text>
+                      <Spacer />
+                      <Button
+                        type={"negative"}
+                        text={"Edit"}
+                        // onPress={onClickEditEntry}
+                      />
+                    </HStack>
+
+                    <VStack>
+                      <VStack top={Spacing[20]}>
+                        <Text type={"body-bold"} style={{ color: Colors.BRIGHT_BLUE }}>
+                          Judul
+                          <Text type={"body-bold"} style={[]}>
+                            {" idenya apa nih?"}
+                          </Text>
+                        </Text>
+                        <TextField
+                          // value={values.title}
+                          // onChangeText={handleChange("title")}
+                          isRequired={false}
+                          // editable={!coachingStore.isDetail}
+                          // isError={isError === "title"}
+                          secureTextEntry={false}
+                          placeholder={"Tulis judul disini"}
+                          charCounter={true}
+                          maxChar={30}
+                          inputStyle={{
+                            backgroundColor: Colors.SOFT_PURPLE,
+                          }}
+                        />
+
+                        <Text type={"body-bold"} style={[]}>
+                          Tulislah
+                          <Text type={"body-bold"} style={{ color: Colors.BRIGHT_BLUE }}>
+                            {" deskripsi "}
+                          </Text>
+                          idemu.
+                        </Text>
+                        <TextField
+                          isRequired={false}
+                          // editable={!coachingStore.isDetail}
+                          // isError={isError === "title"}
+                          secureTextEntry={false}
+                          placeholder={"Tulis deskripsi disini"}
+                          isTextArea
+                          inputStyle={{ minHeight: Spacing[64] }}
+                          charCounter={true}
+                        />
+
+                        <VStack>
+                          <VStack
+                          // style={{bottom: -Spacing[24]}}
+                          >
+                            <Text type={"body-bold"} style={{ color: Colors.BRIGHT_BLUE }}>
+                              Ide
+                              <Text type={"body-bold"} style={[]}>
+                                {" dicetuskan oleh"}
+                              </Text>
+                            </Text>
+                          </VStack>
+                          <TextField
+                            // value={values.title}
+                            // onChangeText={handleChange("title")}
+                            isRequired={false}
+                            // editable={!coachingStore.isDetail}
+                            // isError={isError === "title"}
+                            secureTextEntry={false}
+                          />
+                          {/* <DropDownPicker */}
+                          {/*  items={K_OPTIONS} */}
+                          {/*  // isRequired={true} */}
+                          {/*  // label="Pilih team:" */}
+                          {/*  onValueChange={(value: IOption) => null} */}
+                          {/*  placeholder={"Pilih salah satu"} */}
+                          {/*  containerStyle={{ marginTop: Spacing[4] }} */}
+                          {/*  zIndex={3000} */}
+                          {/*  zIndexInverse={1000} */}
+                          {/*  dropDownDirection={"BOTTOM"} */}
+                          {/* /> */}
+                        </VStack>
+
+                        <VStack horizontal={Spacing[72]} top={Spacing[24]}>
+                          <Button type={"primary"} text={"Submit"} onPress={handleSubmit} />
+                        </VStack>
+                        <HStack>
+                          <Button
+                            type={"primary"}
+                            text={"Vote"}
+                            onPress={handleSubmit}
+                            style={{ width: Spacing[64] }}
+                          />
+                          <Spacer />
+                          <Text type={"body-bold"} style={[]}>
+                            Ide sudah di-vote 7 kali.
+                          </Text>
+                        </HStack>
+                      </VStack>
+                    </VStack>
+
+                    <Spacer height={Spacing[24]} />
+                    <Spacer height={Spacing[24]} />
+                  </VStack>
+                </ScrollView>
+              </SafeAreaView>
+            </>
+          )}
+        </Formik>
       </VStack>
     )
   },
