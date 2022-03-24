11acd7ab5acc9d0f5b02545a800581eb4ea8d6fa
Arda Satata <ardasatata@gmail.com>
2022-03-24 11:30:08 +0800

    added idea button

--- app/screens/homepage/homepage.tsx
+++ app/screens/homepage/homepage.tsx
@@ -59,6 +59,7 @@ import {
   SurprisedInactive,
 } from "@assets/svgs"
 import { MoodComponent } from "./components/mood-component"
+import { BrainstormsComponent } from "@screens/homepage/components/brainstorms-component"
 
 const FEED_EXAMPLE_DATA_ITEM: FeedItemType[] = [
   {
@@ -455,7 +456,7 @@ const Homepage: FC<StackScreenProps<NavigatorParamList, "homepage">> = observer(
             <Spacer height={Spacing[12]} />
             <HomepageCardWrapper animationDuration={700}>
               <VStack>
-                <Button type={"primary"} text={"Brainstorms"} onPress={goToBrainstormsGroup} />
+                <BrainstormsComponent goToBrainstorms={goToJuaraAssessment} />
               </VStack>
             </HomepageCardWrapper>
             <Spacer height={Spacing[12]} />
