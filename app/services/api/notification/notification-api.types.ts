775d7c44301bd6287e78986970679e937226e5c9
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2023-07-07 21:06:53 +0700

    WIP

--- app/services/api/notification/notification-api.types.ts
+++ app/services/api/notification/notification-api.types.ts
@@ -1,6 +1,6 @@
 import { GeneralApiProblem } from "../api-problem"
 
-export type NotificationType = "request_feedback" | "submitted_feedback" | "submitted_comment" | "reaction_feed" | "submited_feedback_user" | "request_feedback_user"
+export type NotificationType = "request_feedback" | "submitted_feedback" | "submitted_comment" | "reaction_feed" | "submited_feedback_user" | "request_feedback_user" | "request_response_journal" | "submitted_response_journal"
 
 export type NotificationItemModel = {
   "notification_id": string;
